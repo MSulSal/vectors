3338b3d66516c6b0b078ae827ef0fc2f2ddf39aa
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-04-06 17:23:40 -0500

    vectorless bouncer

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ const App = () => {
   return (
     <div className="App">
       <h1>Vectors</h1>
-      <div className="canvas-container">
-        <NoVector />
+      <div className="canvas-grid">
+        <div className="canvas-container">
+          <p>Bouncing Ball, No Vectors</p>
+          <NoVector />
+        </div>
       </div>
     </div>
   );
--- src/components/NoVector.jsx
+++ src/components/NoVector.jsx
@@ -1,13 +1,31 @@
 import Sketch from "react-p5";
 
 const NoVector = () => {
+  let x = 100;
+  let y = 100;
+  let xspeed = 2.5;
+  let yspeed = 2;
+
   const setup = (p5, canvasParentRef) => {
     const canvasWidth = canvasParentRef.offsetWidth;
     const canvasHeight = canvasWidth * 0.5;
     p5.background(255);
     p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
   };
-  const draw = () => {};
+  const draw = (p5) => {
+    p5.clear();
+    x += xspeed;
+    y += yspeed;
+    if (x > p5.width || x < 0) {
+      xspeed *= -1;
+    }
+    if (y > p5.height || y < 0) {
+      yspeed *= -1;
+    }
+    p5.stroke(0);
+    p5.fill(127);
+    p5.circle(x, y, 48);
+  };
 
   return <Sketch setup={setup} draw={draw} />;
 };
