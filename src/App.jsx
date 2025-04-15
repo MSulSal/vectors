6a94d7c59abb9e8abba620d2795523520a0dff99
MSulSal <msulsaleem@gmail.com>
2025-04-15 14:05:44 -0500

    fix responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -16,64 +16,60 @@ const App = () => {
   return (
     <div className="App">
       <h1>Vectors</h1>
-      <div className="canvas-grid">
-        <div className="canvas-container">
-          <p>Bouncing ball, no vectors</p>
-          <NoVector />
-        </div>
-        <div className="canvas-container">
-          <p>Bouncing ball, vector</p>
-          <Vector />
-        </div>
-        <div className="canvas-container">
-          <p>Bouncing ball, 3D</p>
-          <Vector3D />
-        </div>
-        <div className="canvas-container">
-          <p>
-            Vector from center to mouse pointer obtained through vector
-            subtraction
-          </p>
-          <Line />
-        </div>
-        <div className="canvas-container">
-          <p>Scaled Vector</p>
-          <Scale />
-        </div>
-        <div className="canvas-container">
-          <p>Bar Representing Magnitude of Vector</p>
-          <Magnitude />
-        </div>
-        <div className="canvas-container">
-          <p>Normalized Vector</p>
-          <Normalize />
-        </div>
-        <div className="canvas-container">
-          <p>Velocity with Vectors</p>
-          <Velocity />
-        </div>
-        <div className="canvas-container">
-          <p>Acceleration with Vectors</p>
-          <Acceleration />
-        </div>
-        <div className="canvas-container">
-          <p>Random Acceleration with Vectors</p>
-          <AccelerationRandom />
-        </div>
-        <div className="canvas-container">
-          <p>Random Acceleration based on Perlin Noise</p>
-          <Perlin />
-        </div>
-        <div className="canvas-container">
-          <p>Acceleration in the direction of the mouse pointer</p>
-          <Mouse />
-        </div>
-        <div className="canvas-container">
-          <p>
-            Gravitational Acceleration in the direction of the mouse pointer
-          </p>
-          <Gravity />
-        </div>
+      <div className="canvas-container">
+        <p>Bouncing ball, no vectors</p>
+        <NoVector />
+      </div>
+      <div className="canvas-container">
+        <p>Bouncing ball, vector</p>
+        <Vector />
+      </div>
+      <div className="canvas-container">
+        <p>Bouncing ball, 3D</p>
+        <Vector3D />
+      </div>
+      <div className="canvas-container">
+        <p>
+          Vector from center to mouse pointer obtained through vector
+          subtraction
+        </p>
+        <Line />
+      </div>
+      <div className="canvas-container">
+        <p>Scaled Vector</p>
+        <Scale />
+      </div>
+      <div className="canvas-container">
+        <p>Bar Representing Magnitude of Vector</p>
+        <Magnitude />
+      </div>
+      <div className="canvas-container">
+        <p>Normalized Vector</p>
+        <Normalize />
+      </div>
+      <div className="canvas-container">
+        <p>Velocity with Vectors</p>
+        <Velocity />
+      </div>
+      <div className="canvas-container">
+        <p>Acceleration with Vectors</p>
+        <Acceleration />
+      </div>
+      <div className="canvas-container">
+        <p>Random Acceleration with Vectors</p>
+        <AccelerationRandom />
+      </div>
+      <div className="canvas-container">
+        <p>Random Acceleration based on Perlin Noise</p>
+        <Perlin />
+      </div>
+      <div className="canvas-container">
+        <p>Acceleration in the direction of the mouse pointer</p>
+        <Mouse />
+      </div>
+      <div className="canvas-container">
+        <p>Gravitational Acceleration in the direction of the mouse pointer</p>
+        <Gravity />
       </div>
     </div>
   );
